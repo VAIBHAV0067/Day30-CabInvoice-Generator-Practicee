1ddae49d6d05369cefd290642797df23ad9e42f8
tutorial6 <manapure.vaibhav542@gmail.com>
2022-04-09 19:23:16 +0530

    initial value

--- CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Gernerator");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+
+            Console.WriteLine(fare);
+        }
+    }
+}
